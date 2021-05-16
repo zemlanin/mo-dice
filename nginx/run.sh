1c98c42856153e75d370be14c8e8f8377a977c6b
Anton Verinov <anton@verinov.com>
2021-05-16 12:47:36 +0300

    remove docker image before running a new one

--- nginx/run.sh
+++ nginx/run.sh
@@ -4,8 +4,10 @@ set -euxo pipefail
 
 cd `dirname $0`
 
+docker rm mo-dice-nginx
+
 docker run \
-  --name mo-dice \
+  --name mo-dice-nginx \
   -p 8088:80 \
   -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro \
   -v $(pwd)/modice.js:/etc/nginx/modice.js:ro \
